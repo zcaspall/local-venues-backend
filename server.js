e8c009ce7d174821cdf8cbe531d90337cf6378c1
zcaspall <zcaspall@hotmail.com>
2023-04-16 21:37:35 -0500

    fixed server issue

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const app = require("./app");
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, '0.0.0.0', () => {
     console.log(`Listening on port: ${process.env.PORT}`);
 })
